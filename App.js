a1153ad97ea64cebe067ee2bf0d6334daff97d27
sagar <sagarkhadka2069@gmail.com>
2021-11-27 16:17:16 +0545

    Forms Added

--- App.js
+++ App.js
@@ -8,11 +8,14 @@ import {
 } from "react-native";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import LoginScreen from "./app/screens/LoginScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-    return <ViewImageScreen />;
+    return <WelcomeScreen />;
 }
--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -2,71 +2,70 @@ import React from "react";
 import { StyleSheet, View, FlatList } from "react-native";
 
 import Screen from "../components/Screen";
-import ListItem from "../components/ListItem";
-import ListItemSeparatorComponent from "../components/ListItemSeparator";
+import { ListItem, ListItemSeparator } from "../components/lists";
 import colors from "../config/colors";
 import Icon from "../components/Icon";
 
 const menuItems = [
-  {
-    title: "My Listings",
-    icon: {
-      name: "format-list-bulleted",
-      backgroundColor: colors.primary,
+    {
+        title: "My Listings",
+        icon: {
+            name: "format-list-bulleted",
+            backgroundColor: colors.primary,
+        },
     },
-  },
-  {
-    title: "My Messages",
-    icon: {
-      name: "email",
-      backgroundColor: colors.secondary,
+    {
+        title: "My Messages",
+        icon: {
+            name: "email",
+            backgroundColor: colors.secondary,
+        },
     },
-  },
 ];
 
 function AccountScreen(props) {
-  return (
-    <Screen style={styles.screen}>
-      <View style={styles.container}>
-        <ListItem
-          title="Mosh Hamedani"
-          subTitle="programmingwithmosh@gmail.com"
-          image={require("../assets/mosh.jpg")}
-        />
-      </View>
-      <View style={styles.container}>
-        <FlatList
-          data={menuItems}
-          keyExtractor={(menuItem) => menuItem.title}
-          ItemSeparatorComponent={ListItemSeparatorComponent}
-          renderItem={({ item }) => (
-            <ListItem
-              title={item.title}
-              IconComponent={
-                <Icon
-                  name={item.icon.name}
-                  backgroundColor={item.icon.backgroundColor}
+    return (
+        <Screen style={styles.screen}>
+            <View style={styles.container}>
+                <ListItem
+                    title="Mosh Hamedani"
+                    subTitle="programmingwithmosh@gmail.com"
+                    image={require("../assets/mosh.jpg")}
                 />
-              }
+            </View>
+            <View style={styles.container}>
+                <FlatList
+                    data={menuItems}
+                    keyExtractor={(menuItem) => menuItem.title}
+                    ItemSeparatorComponent={ListItemSeparator}
+                    renderItem={({ item }) => (
+                        <ListItem
+                            title={item.title}
+                            IconComponent={
+                                <Icon
+                                    name={item.icon.name}
+                                    backgroundColor={item.icon.backgroundColor}
+                                />
+                            }
+                        />
+                    )}
+                />
+            </View>
+            <ListItem
+                title="Log Out"
+                IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
             />
-          )}
-        />
-      </View>
-      <ListItem
-        title="Log Out"
-        IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
-      />
-    </Screen>
-  );
+        </Screen>
+    );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    backgroundColor: colors.light,
-  },
-  container: {
-    marginVertical: 20,
-  },
+    screen: {
+        backgroundColor: colors.light,
+    },
+    container: {
+        marginVertical: 20,
+    },
 });
 
 export default AccountScreen;
