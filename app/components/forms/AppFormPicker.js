85f2e9758389b28a2263d6c1284c343f5adda95d
sagar <sagarkhadka2069@gmail.com>
2021-11-27 17:17:19 +0545

    Picker and Form Adjusted

--- app/components/forms/AppFormPicker.js
+++ app/components/forms/AppFormPicker.js
@@ -1,23 +1,33 @@
 import React from "react";
 import { useFormikContext } from "formik";
 
-import AppPicker from "../AppPicker";
+import Picker from "../AppPicker";
 import ErrorMessage from "./ErrorMessage";
 
-function AppFormPicker({ items, name, placeholder }) {
-  const { errors, setFieldValue, touched, values } = useFormikContext();
+function AppFormPicker({
+    items,
+    name,
+    numberOfColumns,
+    PickerItemComponent,
+    placeholder,
+    width,
+}) {
+    const { errors, setFieldValue, touched, values } = useFormikContext();
 
-  return (
-    <>
-      <AppPicker
-        items={items}
-        onSelectItem={(item) => setFieldValue(name, item)}
-        placeholder={placeholder}
-        selectedItem={values[name]}
-      />
-      <ErrorMessage error={errors[name]} visible={touched[name]} />
-    </>
-  );
+    return (
+        <>
+            <Picker
+                items={items}
+                numberOfColumns={numberOfColumns}
+                onSelectItem={(item) => setFieldValue(name, item)}
+                PickerItemComponent={PickerItemComponent}
+                placeholder={placeholder}
+                selectedItem={values[name]}
+                width={width}
+            />
+            <ErrorMessage error={errors[name]} visible={touched[name]} />
+        </>
+    );
 }
 
 export default AppFormPicker;
